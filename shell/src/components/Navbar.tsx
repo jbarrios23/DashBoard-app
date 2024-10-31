230e04be05f6259c3a6da222f3ad20927f912e57
Javier Barrios <jbarrios.javier@gmail.com>
2024-10-31 13:01:53 -0400

    others

--- shell/src/components/Navbar.tsx
+++ shell/src/components/Navbar.tsx
@@ -4,8 +4,8 @@ import '../App.css';
 
 const Navbar = () => {
   return (
-    <nav style={{ backgroundColor: 'blue', padding: '10px' }}>
-      <h1 style={{ color: 'white' }}>Dashboard Analytics</h1>
+    <nav className="navbar">
+      <h1 className="navbar-logo" >Dashboard Analytics</h1>
       <div className="navbar-links">
         <Link className="navbar-link" to="/">Home</Link>
         <Link className="navbar-link" to="/sales">Ventas</Link>
