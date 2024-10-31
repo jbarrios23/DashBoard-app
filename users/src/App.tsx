c940ecc32a3e52a026513e4e6bbe44280494427c
Javier Barrios <jbarrios.javier@gmail.com>
2024-10-31 13:14:54 -0400

    adjust

--- users/src/App.tsx
+++ users/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <text>Home Users</text>
+        <text>Home Users NEWS</text>
       </header>
     </div>
   );
