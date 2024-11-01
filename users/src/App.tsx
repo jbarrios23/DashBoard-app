974f7c6e937473d08ee1ff506775dcbde15e7188
Javier Barrios <jbarrios.javier@gmail.com>
2024-11-01 18:16:18 -0400

    put user stuff :apple:

--- users/src/App.tsx
+++ users/src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UsersModule from './pages/UsersModule';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <text>Home Users NEWS</text>
-      </header>
-    </div>
+    <Router>
+    <Routes>
+      <Route path="/users" element={<UsersModule />} />
+    </Routes>
+  </Router>
   );
 }
 
