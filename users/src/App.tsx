4a85a5117b07380a6b52249c397c74c5fc1cf75c
Javier Barrios <jbarrios.javier@gmail.com>
2024-11-02 10:35:12 -0400

    little adjust :apple:

--- users/src/App.tsx
+++ users/src/App.tsx
@@ -1,15 +1,14 @@
-
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import UsersModule from './pages/UsersModule';
 
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route path="/users" element={<UsersModule />} />
-    </Routes>
-  </Router>
+      <Routes>
+        <Route path="/users" element={<UsersModule />} />
+      </Routes>
+    </Router>
   );
 }
 
